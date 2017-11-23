c9021e69067618cc2b788636c444473d17d5b950
wangye9323 <wangye9323@autohome.com.cn>
2017-11-23 15:04:31 +0800

    关于html5的video的小demo

---
+++ js/json.js
@@ -0,0 +1,58 @@
+var data = {
+  "message":"",
+  "result":{
+      "numFound":"5",
+      "dataList": {
+            "title":"北京",
+            "date":"2014-05-18 22:27:29",
+            "headImg":"https://wx1.sinaimg.cn/large/9c0f9a41ly1flppuicplsj20ru0fo0us.jpg",
+            "userName":"nick222222",
+            "viewCount":"8",
+            "dataSource": [
+                {
+                  "type": "video",
+                  "playTime": "12",
+                  "address": "北京市海淀区",
+                  "startPoint": "30,30",
+                  "endPoint": "100,60",
+                  "tagStartTime": "0.10",
+                  "tagEndTime": "2.20",
+                  "tagRotate": "-30",
+                  "tagUrl": "//krcom.cn/",
+                  "imgUrl": "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1511345622601&di=94b8705f7dbf832d0b39c4f42524c13e&imgtype=0&src=http%3A%2F%2Ff.hiphotos.baidu.com%2Fimage%2Fpic%2Fitem%2Fd788d43f8794a4c2688360c704f41bd5ac6e39bd.jpg",
+                  "videoUrl": "http://www.runoob.com/try/demo_source/movie.mp4",
+                  "posterUrl": "https://wx1.sinaimg.cn/crop.72.0.1111.618/005Md8mYgy1flqoiy1hvfj31420h6n2r.jpg?1"
+                },
+                {
+                  "type": "video",
+                  "playTime": "3",
+                  "address": "北京市海淀区",
+                  "startPoint": "30,30",
+                  "endPoint": "100,60",
+                  "tagStartTime": "0.10",
+                  "tagEndTime": "2.20",
+                  "tagRotate": "-30",
+                  "tagUrl": "//krcom.cn/",
+                  "imgUrl": "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1511345622601&di=94b8705f7dbf832d0b39c4f42524c13e&imgtype=0&src=http%3A%2F%2Ff.hiphotos.baidu.com%2Fimage%2Fpic%2Fitem%2Fd788d43f8794a4c2688360c704f41bd5ac6e39bd.jpg",
+                  "videoUrl": "http://us.cdn.krcom.cn/000PpteHjx07fFnYFSYw0104010025D60k01.mp4?label=mp4_hd&template=25&Expires=1511422630&ssig=ra74VoD2XR&KID=unistore,video",
+                  "posterUrl": "https://wx1.sinaimg.cn/crop.72.0.1111.618/005Md8mYgy1flqoiy1hvfj31420h6n2r.jpg?2"
+                },
+                {
+                  "type": "video",
+                  "playTime": "12",
+                  "address": "北京市海淀区",
+                  "startPoint": "30,30",
+                  "endPoint": "100,60",
+                  "tagStartTime": "0.10",
+                  "tagEndTime": "2.20",
+                  "tagRotate": "-30",
+                  "tagUrl": "//krcom.cn/",
+                  "imgUrl": "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1511345622601&di=94b8705f7dbf832d0b39c4f42524c13e&imgtype=0&src=http%3A%2F%2Ff.hiphotos.baidu.com%2Fimage%2Fpic%2Fitem%2Fd788d43f8794a4c2688360c704f41bd5ac6e39bd.jpg",
+                  "videoUrl": "http://www.runoob.com/try/demo_source/movie.mp4",
+                  "posterUrl": "https://wx1.sinaimg.cn/crop.72.0.1111.618/005Md8mYgy1flqoiy1hvfj31420h6n2r.jpg?1"
+                }
+            ]
+        }
+  },
+  "returnCode":0
+}
