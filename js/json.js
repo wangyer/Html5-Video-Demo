d219b51305f6510291995183e834d7b0956745ae
wangye9323 <wangye9323@autohome.com.cn>
2017-11-27 17:31:18 +0800

    视频进度及播放优化可支持手机端浏览

--- js/json.js
+++ js/json.js
@@ -25,7 +25,7 @@ var data = {
                 },
                 {
                   "type": "video",
-                  "playTime": "3",
+                  "playTime": "155",
                   "address": "北京市海淀区",
                   "startPoint": "30,30",
                   "endPoint": "100,60",
@@ -34,7 +34,7 @@ var data = {
                   "tagRotate": "-30",
                   "tagUrl": "//krcom.cn/",
                   "imgUrl": "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1511345622601&di=94b8705f7dbf832d0b39c4f42524c13e&imgtype=0&src=http%3A%2F%2Ff.hiphotos.baidu.com%2Fimage%2Fpic%2Fitem%2Fd788d43f8794a4c2688360c704f41bd5ac6e39bd.jpg",
-                  "videoUrl": "http://us.cdn.krcom.cn/000PpteHjx07fFnYFSYw0104010025D60k01.mp4?label=mp4_hd&template=25&Expires=1511422630&ssig=ra74VoD2XR&KID=unistore,video",
+                  "videoUrl": "http://fus.cdn.krcom.cn/004kWezrlx07g29qHYU001040100m4NS0k01.mp4?label=mp4_hd&template=25&Expires=1511777211&ssig=oW9279ryTX&KID=unistore,video",
                   "posterUrl": "https://wx1.sinaimg.cn/crop.72.0.1111.618/005Md8mYgy1flqoiy1hvfj31420h6n2r.jpg?2"
                 },
                 {
